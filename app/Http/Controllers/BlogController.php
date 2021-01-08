9a3d7ce3b2fa65c64640661367fd75bed5fcf5d3
kako_ogura <your.email@example.com>
2021-01-08 17:45:53 +0900

    Eloquentでデータを取り出す処理

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use phpDocumentor\Reflection\Utils;
+use App\Models\Blog;
 
 class BlogController extends Controller
 {
     public function showList()
     {
-        return view('blog.list');
+        $blogs = Blog::all();
+        return view('blog.list',['blogs' => $blogs]);
     }
 }
